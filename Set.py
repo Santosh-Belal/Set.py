2088df212d26de7066fe6aca0c34b549eed9e677
Santosh Belal <107342749+Santosh-Belal@users.noreply.github.com>
2022-11-07 05:31:48 +0545

    Create Set.py

---
+++ Set.py
@@ -0,0 +1,29 @@
+num1={ 1,2,3,5,6,7,8,9}
+
+num2= {10,2,30,12,15,16,19}
+
+num3 ={12,13,15,20,32,20,24,60,20}
+
+print(2 in num1)
+
+print(num1 | num2 | num3)
+
+print( num1 | num2)
+
+print(num3 & num2)
+
+print(num1 - num2)
+
+print(num3 - num2)
+
+print(num2 & num1)
+
+print(num1^num2)
+
+print(len(num1))
+
+print(len(num3))
+
+print(num3)
+
+print((num1))
